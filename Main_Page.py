e094a2274e7409c0397da02251b896c70590c1bf
LynxisKhat <kaungkhantheinkyaw@outlook.com>
2022-10-28 14:14:33 +0630

    second commit

---
+++ Main_Page.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+from PIL import Image
+
+
+st.title('Netflix Movies and TV Show ')
+
+netflix_data = pd.read_csv('netflix_titles.csv')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(netflix_data)
+
+area_data = netflix_data.groupby('type').count()
+
+st.write(area_data)
+
+image = Image.open('countplot.png')
+
+st.image(image, caption='CountPlot Data')
---
+++ pages/About.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.markdown("# Assigment 3 - Using Streamlit")
+st.sidebar.markdown("# Assigment 3")
